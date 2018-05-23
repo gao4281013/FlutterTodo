f54e32aca56563a5283df52bcca868eb41a8cd30
gavin <yongfeng.gao@pactera.com>
2018-05-23 18:03:29 +0800

    code custom appbar

--- flutter_app/lib/CostumAppbar.dart
+++ flutter_app/lib/CostumAppbar.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+const double _kLeadingWidtj = kToolbarHeight;
+const Object _kDefaultHeroTag = const Object();
+const Object _kUnspecifiedArgument = const Object();
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget{
+
+
+  @override
+  Size get preferredSize {
+
+  }
+
+  @override
+  State createState() {
+
+  }
+}
