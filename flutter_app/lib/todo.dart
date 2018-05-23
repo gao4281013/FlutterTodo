7d4c92427ec56f15f9b41d6f0034f376dfbc6708
gavin <yongfeng.gao@pactera.com>
2018-05-23 10:36:08 +0800

    finish bean

--- flutter_app/lib/todo.dart
+++ flutter_app/lib/todo.dart
@@ -1,1 +1,41 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
+import 'dart:math';
+import 'dart:collection';
+import 'package:uuid/uuid.dart';
+
+class TodoObject{
+
+  String uuid;
+  int sortID;
+  String title;
+  Color color;
+  IconData icon;
+  Map<DateTime,List<Task>>
+}
+
+
+class TaskObject{
+  DateTime date;
+  String task;
+  bool _completed;
+
+  TaskObject(this.date, this.task, this._completed){
+    this.date = date;
+    this.task = task;
+    this._completed = _completed;
+  }
+
+  TaskObject.import(String task,DateTime date,bool completed){
+    this.task = task;
+    this.date = date;
+    this._completed = completed;
+  }
+
+  set completed(bool value) {
+    _completed = value;
+  }
+
+  isCompleted() => _completed;
+
+}
