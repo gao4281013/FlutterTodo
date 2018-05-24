2a0057cb3c660d5d9a1f8ff83169f379da8d3e25
gavin <yongfeng.gao@pactera.com>
2018-05-24 16:22:42 +0800

    code main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -84,19 +84,52 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  ScrollController scrollController;
+  Color backgroundColor;
+  Tween<Color> colorTween;
+  int currentPage = 0;
+  Color constBackColor;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+  @override
+  void initState() {
+    colorTween = new ColorTween( begin: ColorChoies.colors[0],end: ColorChoies.colors[1]);
+    backgroundColor = todos[0].color;
+    scrollController = new ScrollController();
+    scrollController.addListener((){
+      ScrollPosition position = scrollController.position;
+      ScrollDirection direction = position.userScrollDirection;
+      int page = (position.pixels / (position.maxScrollExtent/(todos.length.toDouble() -1 ))).toInt();
+      double pageDo = (position.pixels /(position.maxScrollExtent/(todos.length.toDouble() -1)));
+      double percent = pageDo -page;
+
+      if(direction == ScrollDirection.reverse){
+        if(todos.length -1 < page+1){
+          return;
+        }
+        colorTween.begin = todos[page].color;
+        colorTween.end = todos[page+1].color;
+        setState(() {
+          backgroundColor = colorTween.lerp(percent);
+        });
+      }else if (direction == ScrollDirection.forward){
+        if(todos.length -1 <page+1){
+          return;
+        }
+        colorTween.begin = todos[page].color;
+        colorTween.end = todos[page+1].color;
+        setState(() {
+          backgroundColor = colorTween.lerp(percent);
+        });
+
+      }else {
+        return;
+      }
     });
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -106,51 +139,185 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+    final double _width = MediaQuery.of(context).size.width;
+    final double _ratioW = _width/375.0;
+
+    final double _height = MediaQuery.of(context).size.height;
+    final double _ratioH = _height/812.0;
+
+    return new Container(
+      decoration: new BoxDecoration(
+        color: backgroundColor
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+      child: new Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: new AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: new Text("TODO"),
+          leading: new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: (){}
+              ),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(CustomIcons.search,size: 26.0),
+                onPressed: (){}
+                ),
           ],
         ),
+        body: new Container(
+          child: new Stack(
+            children: <Widget>[
+              new Container(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Container(
+                      padding: const EdgeInsets.only(top: 20.0,bottom: 20.0,left: 50.0,right: 60.0),
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Padding(
+                            padding: const EdgeInsets.only(bottom: 25.0),
+                            child: new Container(
+                              decoration: new BoxDecoration(
+                                boxShadow: [
+                                  new BoxShadow(
+                                    color: Colors.black38,
+                                    offset: new Offset(5.0, 5.0),
+                                    blurRadius: 15.0,
+                                  ),
+                                ],
+                                shape: BoxShape.circle,
+                              ),
+                              child: new CircleAvatar(
+                                backgroundColor: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          new Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                          child: new Text(
+                            "Hello, Gavin",
+                            style: new TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                            ),
+                          ),
+                          ),
+                          new Text(
+                            "This is a daily quote",
+                            style: new TextStyle(
+                              color: Colors.white70,
+                            ),
+                          ),
+                          new Text(
+                            "You have 10 tasks to do today",
+                              style: new TextStyle(
+                                color: Colors.white70,
+                              ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    new Container(
+                      height: 350.0,
+                      width: _width,
+                      child: new ListView.builder(
+                          itemBuilder: (context,index){
+                            TodoObject todoObject = todos[index];
+                            EdgeInsets padding = const EdgeInsets.only(left: 10.0,right: 10.0,top: 20.0,bottom: 30.0);
+
+                            double percentComplete = todoObject.percentComplete();
+
+                            return new Padding(
+                                padding: padding,
+                                child: new InkWell(
+                                  onTap: (){
+                                    Navigator.of(context).push(new PageRouteBuilder(
+                                        pageBuilder: (BuildContext context,Animation<double> animation,Animation<double> secondaryAnimation) => new DetailPage(todoObject: todoObject),
+                                        transitionsBuilder: (BuildContext context,Animation<double> animation,Animation<double> secondaryAnimation,Widget child){
+                                          return new SlideTransition(
+                                              position: new Tween<Offset>(
+                                                 begin: const Offset(0.0,1.0),
+                                                 end: Offset.zero,
+                                              ).animate(animation),
+                                              child: new SlideTransition(
+                                                  position: new Tween<Offset>(
+                                                    begin: Offset.zero,
+                                                    end: const Offset(0.0, 1.0),
+                                                  ).animate(secondaryAnimation),
+                                                child: child,
+                                              ),
+                                          );
+                                        },
+                                      transitionDuration: const Duration(milliseconds: 1000)
+                                    ),);
+                                  },
+                                 child: new Container(
+                                   decoration: new BoxDecoration(
+                                     borderRadius: new BorderRadius.circular(10.0),
+                                     boxShadow: [
+                                       new BoxShadow(
+                                         color: Colors.black.withAlpha(70),
+                                         offset: const Offset(3.0, 10.0),
+                                         blurRadius: 15.0
+                                       )
+                                     ]
+                                   ),
+                                   height: 250.0,
+                                   child: new Stack(
+                                     children: <Widget>[
+                                       new Hero(
+                                           tag: todoObject.uuid + "_background",
+                                           child: new Container(
+                                               decoration: new BoxDecoration(
+                                                 color: Colors.white,
+                                                 borderRadius: new BorderRadius.circular(10.0),
+                                               ),
+                                           )
+                                       ),
+                                       new Padding(
+                                           padding: null),
+                                     ],
+                                   ),
+                                 ),
+                                ),
+                            );
+                          }),
+
+                    ),
+                  ],
+                ),
+
+              ),
+              new Padding(
+                  padding: const EdgeInsets.only(right: 15.0,bottom: 15.0),
+                  child: new Align(
+                    alignment: Alignment.bottomRight,
+                    child: new FloatingActionButton(
+                        onPressed: (){},
+                        tooltip: 'Increment',
+                        child: new Icon(Icons.add),
+                    ),
+                  ),
+              ),
+            ],
+          ),
+
+        ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
 class DetailPage extends StatefulWidget {
+  final TodoObject todoObject;
+
+
+  DetailPage({@required this.todoObject,Key key}):super(key:key);
+
   @override
   _DetailPageState createState() => new _DetailPageState();
 }
